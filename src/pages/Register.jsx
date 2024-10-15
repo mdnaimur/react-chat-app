d7b13e5ce1e6d6d38c8dd8ea8f8265e43de9b0bf
mdnaimur <mdnaimurr@gmail.com>
2024-10-16 00:35:55 +0600

    start chat app

---
+++ src/pages/Register.jsx
@@ -0,0 +1,19 @@
+const Register = () => {
+  return (
+    <div>
+      <div>
+        <span> Chat App </span>
+        <span> Register </span>
+        <form>
+          <input type="text" placeholder="enter your name " />
+          <input type="email" placeholder="enter your email" />
+          <input type="password" placeholder="enater your password" />
+          <input type="file" />
+        </form>
+        <p> You do have an account? Login </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
