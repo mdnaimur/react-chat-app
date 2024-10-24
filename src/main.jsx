a67c394d83eaaf6bbee1481408a72f4b4696f4f0
mdnaimur <mdnaimurr@gmail.com>
2024-10-24 17:08:44 +0600

    bug fix and loging done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { AuthContextProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </StrictMode>
 );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,33 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../firebase";
+
 const Login = () => {
+  const [error, SetError] = useState(false);
+  const navigate = useNavigate();
+
+  const hanldeSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      SetError(true);
+    }
+  };
+
   return (
     <div className=" min-h-[100vh] flex justify-center items-center p-4  bg-slate-600  ">
       <div className="flex flex-col items-center gap-3 bg-white rounded-lg py-8 px-16  shadow-lg">
         <span className="text-3xl font-bold text-yellow-400"> Chat App </span>
         <span className="text-2xl mb-10"> LogIn </span>
-        <form className="flex flex-col gap-4 w-full">
+        <form className="flex flex-col gap-4 w-full" onSubmit={hanldeSubmit}>
           <input
             type="email"
             placeholder="Enter Your Email"
@@ -19,10 +42,13 @@ const Login = () => {
           <button className="w-full bg-yellow-500 p-3 rounded-lg font-bold hover:bg-yellow-700 transition">
             Sing In
           </button>
+          {error && (
+            <span className="text-red-600">User or password is incorrect</span>
+          )}
         </form>
         <p className="w-full text-md">
           You don&apos;t have an account?
-          {/* <Link to="/">Login</Link> */}
+          <Link to="/register">Register</Link>
         </p>
       </div>
     </div>
